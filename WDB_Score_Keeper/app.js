1d2633b5cb243caee8587e6acf1e904476d51298
Jaimin Parmar <jaimins100@gmail.com>
2022-12-11 15:15:39 +0530

    score keeper w/ bulma styling

--- WDB_Score_Keeper/app.js
+++ WDB_Score_Keeper/app.js
@@ -7,7 +7,7 @@ const winningScoreSelect = document.querySelector("#playto");
 
 let p1Score = 0;
 let p2Score = 0;
-let winningScore = 5;
+let winningScore = parseInt(winningScoreSelect.value);
 let isGameOver = false;
 
 p1Button.addEventListener("click", () => {
@@ -16,8 +16,10 @@ p1Button.addEventListener("click", () => {
     p1Display.textContent = p1Score;
     if (p1Score == winningScore) {
       isGameOver = true;
-      p1Display.classList.add("winner");
-      p2Display.classList.add("loser");
+      p1Display.classList.add("has-text-success");
+      p2Display.classList.add("has-text-danger");
+      p1Button.disabled = true;
+      p2Button.disabled = true;
     }
   }
 });
@@ -28,17 +30,12 @@ p2Button.addEventListener("click", () => {
     p2Display.textContent = p2Score;
     if (p2Score == winningScore) {
       isGameOver = true;
-      p2Display.classList.add("winner");
-      p1Display.classList.add("loser");
+      p2Display.classList.add("has-text-success");
+      p1Display.classList.add("has-text-danger");
+      p1Button.disabled = true;
+      p2Button.disabled = true;
     }
   }
-});
-
-resetButton.addEventListener("click", reset);
-
-winningScoreSelect.addEventListener("change", () => {
-  winningScore = parseInt(this.value);
-  reset();
 });
 
 const reset = () => {
@@ -47,6 +44,15 @@ const reset = () => {
   p2Score = 0;
   p1Display.textContent = p1Score;
   p2Display.textContent = p2Score;
-  p1Display.classList.remove("winner", "loser");
-  p2Display.classList.remove("winner", "loser");
+  p1Display.classList.remove("has-text-success", "has-text-danger");
+  p2Display.classList.remove("has-text-success", "has-text-danger");
+  p1Button.disabled = false;
+  p2Button.disabled = false;
 };
+
+resetButton.addEventListener("click", reset);
+
+winningScoreSelect.addEventListener("change", () => {
+  winningScore = parseInt(this.value);
+  reset();
+});
