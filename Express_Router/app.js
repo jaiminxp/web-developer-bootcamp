cf6933f93f17329079a37dfd3f122382b021ae73
Jaimin Parmar <jaimins100@gmail.com>
2023-02-20 12:17:42 +0530

    router middleware

--- Express_Router/app.js
+++ Express_Router/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const shelterRoutes = require('./routes/shelters');
+const adminRoutes = require('./routes/admin');
 
-app.use('/', shelterRoutes);
+app.use('/shelters', shelterRoutes);
+app.use('/admin', adminRoutes);
 
 app.listen(3000, () => {
   console.log('Serving app on localhost:3000');
